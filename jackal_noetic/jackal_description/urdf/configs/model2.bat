66af2d1f7b91efec67e4bef45f1493ae03aacdb3
youngjae <k.youngj2528@gmail.com>
2023-11-12 08:50:23 -0500

    First commit

---
+++ jackal_noetic/jackal_description/urdf/configs/model2.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM The front_laser configuration of Jackal is sufficient for
+REM basic gmapping and navigation. It is mostly the default
+REM config, but with a SICK LMS100 series LIDAR on the front,
+REM pointing forward.
+
+set MODEL2=1
+
+
